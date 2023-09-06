7fdb68897996416c97df78e9430d018099b0b16c
humy06001 <humy06001@E-PW0234KS>
2023-09-06 13:07:29 +0200

    Add project files.

---
+++ Kap 1 Uppgift 1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skriver ut triangeln
+            Console.WriteLine(
+                "   /\\ \n" + //\n skapar en ny rad i texten
+                "  /  \\ \n" +
+                " /    \\ \n" +
+                "/______\\ \n"
+                );
+
+            //Läser av en tangent
+            Console.ReadKey();
+        }
+    }
+}
